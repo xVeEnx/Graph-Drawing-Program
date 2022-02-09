8669655daabba514b8b776763bf9e19e14c4fe88
Dawid Paszkot <Negs@op.pl>
2022-02-10 00:06:05 +0100

    Zrobic osobna klase pod Labele i przeniesc rysowanie do niej zmienic wysokosc ogolna labeli i powinno byc ok

--- graphspace.cpp
+++ graphspace.cpp
@@ -3,39 +3,45 @@
 graphSpace::graphSpace(QWidget *parent)
     : QWidget{parent}
 {
+    setContentsMargins(0,0,0,0);
     _iWidth=15;
     _iSpace=5;
     _iScale=100;
+    auto _qMainLayout=new QVBoxLayout();
+    double height=this->height()/10;
+    _qMainLayout->setSpacing(40);
 
-    for(int i=5;i>0;i--)
+    qDebug()<<height;
+    for(int i=10; i>=0; i--)
     {
         double iscl=_iScale*0.05*i;
         QString str=QString::number(iscl);
-            //_qMainLayout->addWidget(new QLabel(str),i,0);
-            _qLinia.push_back(QLine(0,QWidget::height()/10*i,50,QWidget::height()/10*i)); //cos spacing w gridzie wali
-            _qLinia.push_back(QLine(20*i,QWidget::height()/10*i,20*i,0)); //linie pomocnicze sprawdzam wysokosci
+        _qMainLayout->addWidget(new QLabel(str));
+        _qLinia.push_back(QLine(0,55*i+1,1001,55*i+1));
     }
+    _qMainLayout->addItem(new QSpacerItem(0, 0, QSizePolicy::Fixed, QSizePolicy::Expanding));
+    setLayout(_qMainLayout);
 }
 void graphSpace::addGraphs()
 {
     int height=QWidget::height();
-     GraphManaging* manager = qobject_cast<GraphManaging* >(sender()->parent());
-     int graphHeight=(manager->getGraphHeight()).toInt();
+    GraphManaging* manager = qobject_cast<GraphManaging* >(sender()->parent());
+    int graphHeight=(manager->getGraphHeight()).toInt();
 
-     if(graphHeight<1) return;
+    if(graphHeight<1) return;
 
-     qDebug()<<graphHeight;
+    qDebug()<<graphHeight;
 
-     //_qLinia;
-     manager->clearGraphHeight();
+    //_qLinia;
+    manager->clearGraphHeight();
     int numOfGraphs= graphsVect.size();
     graphsVect.push_back(QRect(_iSpace*numOfGraphs+_iWidth*numOfGraphs,height-1,_iWidth,-graphHeight));
     update();
 }
 void graphSpace::paintEvent(QPaintEvent *event)
 {
-QPainter painter(this);
-for(int i=0; i<graphsVect.size(); i++)painter.drawRect(graphsVect[i]);
+    QPainter painter(this);
+    for(int i=0; i<graphsVect.size(); i++) painter.drawRect(graphsVect[i]);
 
-for(int i=0;i<_qLinia.size();i++) painter.drawLine(_qLinia[i]);
+    for(int i=0; i<_qLinia.size(); i++) painter.drawLine(_qLinia[i]);
 }
